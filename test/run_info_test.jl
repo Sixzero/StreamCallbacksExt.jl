ffef9e8cb635572314558fe584f6ced3c3819b54
SixZero <havliktomi@hotmail.com>
2025-02-12 10:44:13 +0100

    Fixing CI

--- test/run_info_test.jl
+++ test/run_info_test.jl
@@ -1,9 +1,12 @@
+using StreamCallbacksExt: needs_tool_execution, RunInfo
+using Test
+
 @testset "RunInfo" begin
     info = RunInfo()
     @test !needs_tool_execution(info)
     
     info.stop_sequence = "STOP"
-    @test !needs_tool_execution(info)
+    @test needs_tool_execution(info)
     
     info.stop_sequence = "stop"
     @test needs_tool_execution(info)
